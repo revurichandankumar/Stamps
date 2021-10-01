b6f6f237311735991e55271c1697df02374ade86
Chandan kumar <chandan.r@oneposretail.com>
2021-10-01 15:41:50 +0530

    changes

--- Models/AddZones.cs
+++ Models/AddZones.cs
@@ -26,4 +26,17 @@ namespace OneposStamps.Models
         public string Id { get; set; }
         public string Name { get; set; }
     }
+    public class InsertZones
+    {
+        public string ZoneName { get; set; }
+        public string  CarrierId { get; set; }
+        public decimal ShipMentFee { get; set; }
+        public decimal Weight { get; set; }
+        public string ServiceTypeId { get; set; }
+        public string PackingId { get; set; }
+        public decimal Length { get; set; }
+        public decimal Breadth { get; set; }
+        public decimal Height { get; set; }
+        public string Store_Id { get; set; }
+    }
 }
