9926123340020647c8acdfedbd2e6eacefe98d41
vaibhavmistry201 <vaibhav.mistry@oneposretail.com>
2021-10-05 10:30:09 +0530

    zip code set up

--- Controllers/ZoneController.cs
+++ Controllers/ZoneController.cs
@@ -220,35 +220,7 @@ namespace OneposStamps.Controllers
             return View();
         }
 
-        public ActionResult GetZipDefaultData()
-        {
-            DataSet ds = db.GetCarrierdata("USP_GetZipcodesDefaultData");
-            GetZipCodeData ZipData = new GetZipCodeData();
-            if (ds.Tables.Count > 0)
-            {
-                List<ZipCodes> Gz = new List<ZipCodes>();
-                foreach (DataRow row in ds.Tables[0].Rows)
-                {
-                    ZipCodes a = new ZipCodes();
-                    a.Name = (row["primary_city"]).ToString();
-                    a.Zipcode = (row["zip"]).ToString();
-                    Gz.Add(a);
-                }
-                ZipData.ZipCodeList = Gz;
-
-                var groupedCustomerList = Gz.GroupBy(u => u.Name).Select(grp => grp.ToList()).ToList();
-                //foreach(ZipData.ZipCodeList a in groupedCustomerList)
-                //{
-
-                //}
-                
-
-                //var json = JsonConvert.SerializeObject(groupedCustomerList);
-
-
-            }
-            return View();
-        }
+       
     }
     public class zipvalue
     {
